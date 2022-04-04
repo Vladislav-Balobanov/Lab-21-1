155cab3cfc001a7723386801aa34399578884d7d
Владислав Балобанов <slava_balobanov@mail.ru>
2022-04-05 00:42:14 +0700

    Объявлены методы класса.

--- Lab-21-1/CTemplStack.h
+++ Lab-21-1/CTemplStack.h
@@ -1,12 +1,38 @@
 #pragma once
 #include <iostream>
 
+/// <summary>
+/// Класс реализующий стэк.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 template <class T>
 class CTemplStack
 {
 public:
+	// Конструктор по умолчанию:
+	CTemplStack();
+	// Конструктор по размеру стэка:
+	CTemplStack(size_t size = 1);
+	// Деструктор:
+	~CTemplStack();
 
+	///////////////////////////
+	// Методы работы со стэком:
+	///////////////////////////
+
+	// @Short
+	// Добавление в стэк:
+	bool addToStack(T object);
+	// Удаление из стэка:
+	bool delFromStack(T object);
+	// Получить размер стэка:
+	size_t getSizeOfStack();
+protected:
+	void createObject();
 private:
-	T object;
+	// Объект в стэке:
+	T _object;
+	// Размер стэка:
+	size_t _size;
 };
 
