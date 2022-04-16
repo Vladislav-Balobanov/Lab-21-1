5cec8fdaea70fc95f251cd5714468725d819d2e7
Владислав Балобанов <slava_balobanov@mail.ru>
2022-04-16 12:54:18 +0700

    Запилины классы исключений стека + реализован базовый класс.

--- Lab-21-1/EStackException.h
+++ Lab-21-1/EStackException.h
@@ -3,12 +3,26 @@
 #include <iostream>
 class EStackException
 {
-	
-;
+	std::string message;
+public:
+	EStackException(std::string _message)
+	{
+		message = _message;
+	}
+	EStackException(const EStackException& exception)
+	{
+		message = exception.message;
+	}
+	~EStackException()
+	{
+		delete &message;
+	}
+
+	const std::string what () const { return message; }
 };
 
  class EStackEmpty : public EStackException
 {
-
+	
 };
 
